fe8173dc2229ec197cf0c65d668225db01bcb9bd
Ehsanullah Farooqi <ehsan.farooqi12@gmail.com>
2024-02-29 22:08:59 +0100

    Implement paments with stripe

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -1,6 +1,44 @@
+const Stripe = require('stripe');
+
 const Booking = require('../models/bookingModel');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/apiFeuter');
 
-exports.getCheckoutSession = catchAsync(async (req, res, next) => {});
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1) Get the currently booked tour
+  const tour = await Tour.findById(req.params.tourId);
+
+  // 2) Create Checkout session
+  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+        },
+      },
+    ],
+    mode: 'payment',
+  });
+
+  // 3) Create Session as response
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
