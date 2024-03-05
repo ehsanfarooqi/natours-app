41f375f33dbe59f579ea012eba3a5d0a2d1d59a2
Ehsan Farooqi <ehsan.farooqi12@gmail.con>
2024-03-05 22:51:19 +0100

    Booking Tour

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -6,7 +6,6 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/apiFeuter');
 const factory = require('./handlerFactory');
-const { signature } = require('stripe/lib/Webhooks');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
@@ -19,7 +18,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-
     success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
