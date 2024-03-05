03966b82dc35bc97d1825c6a7d9a5ec817ddaa13
Ehsan Farooqi <ehsan.farooqi12@gmail.con>
2024-03-05 22:19:02 +0100

    Create webhook

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -84,7 +84,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+    createbookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
 };
