5ccf91cff877c33706fc4dd6a114fb7f346e116a
Ehsanuööah Farooqi <ehsan.farooqi12@gmail.com>
2024-04-22 11:04:20 +0200

    Admin settings

--- controller/viewCotroller.js
+++ controller/viewCotroller.js
@@ -157,8 +157,10 @@ exports.getResetPassForm = catchAsync(async (req, res, next) => {
 
 // Manage Tours Page
 exports.getMangeTour = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
   res.status(200).render('manageTours', {
     title: 'Manage Tours',
+    tours,
   });
 });
 
